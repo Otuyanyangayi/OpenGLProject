c58120b82203bc7a46dcf307608f50bf8c14f8a1
otuyanyangayi <gracenyangayi@gmail.com>
2024-10-22 16:44:05 -0400

    drawing a simple cube

--- moving_shapes.rb
+++ moving_shapes.rb
@@ -5,8 +5,27 @@ require 'glut'
 include OpenGL
 include GLUT 
 
+puts "Start"
+
 def init 
     glClearColor(0,0,0,0) #make the background colr black
     glEnable(GL_DEPTH_TEST) #Enable 3D depth testing 
 end 
 
+def draw_cube
+    glBegin(GL_QUADS)  # Start drawing a cube using quads
+  
+    # Define vertices for each face of the cube
+    # Front face
+    glColor3f(1.0, 0.0, 0.0)  # Red color
+    glVertex3f(-1.0, -1.0, 1.0)
+    glVertex3f(1.0, -1.0, 1.0)
+    glVertex3f(1.0, 1.0, 1.0)
+    glVertex3f(-1.0, 1.0, 1.0)
+  
+    
+  
+    glEnd
+  end
+
+  puts "Done"
