0faf0515f9ef1bcecfdd9e83d81e04020fb45ca9
otuyanyangayi <gracenyangayi@gmail.com>
2024-10-23 02:07:49 -0400

    add more faces to cube

--- moving_shapes.rb
+++ moving_shapes.rb
@@ -4,7 +4,9 @@ require 'glut'
 include OpenGL
 include GLUT
 
-$angle = 0.0  # Angle of rotation
+$angle = 0.0
+$x_move = 0.0
+$y_move = 0.0
 
 def init
   glClearColor(0.0, 0.0, 0.0, 0.0)  # Set background color to black
@@ -12,74 +14,39 @@ def init
 end
 
 def display
-  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)  # Clear the screen and depth buffer
-  glLoadIdentity  # Reset transformations
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+  glLoadIdentity
 
-  # Move the object along the Z-axis
-  glTranslatef(0.0, 0.0, -5.0)
+  # Apply translation based on keyboard input
+  glTranslatef($x_move, $y_move, -5.0)
 
-  # Rotate the cube
+  # Apply rotation
   glRotatef($angle, 1.0, 1.0, 0.0)
 
   draw_cube
 
-  glutSwapBuffers  # Swap the front and back buffers
+  glutSwapBuffers
 end
 
 def draw_cube
-  glBegin(GL_QUADS)  # Start drawing a cube using quads
+  glBegin(GL_QUADS)
 
   # Front face
-  glColor3f(1.0, 0.0, 0.0)  # Red
+  glColor3f(1.0, 0.0, 0.0)  # Red color
   glVertex3f(-1.0, -1.0, 1.0)
   glVertex3f(1.0, -1.0, 1.0)
   glVertex3f(1.0, 1.0, 1.0)
   glVertex3f(-1.0, 1.0, 1.0)
 
-  # Back face
-  glColor3f(0.0, 1.0, 0.0)  # Green
-  glVertex3f(-1.0, -1.0, -1.0)
-  glVertex3f(-1.0, 1.0, -1.0)
-  glVertex3f(1.0, 1.0, -1.0)
-  glVertex3f(1.0, -1.0, -1.0)
-
-  # Left face
-  glColor3f(0.0, 0.0, 1.0)  # Blue
-  glVertex3f(-1.0, -1.0, -1.0)
-  glVertex3f(-1.0, -1.0, 1.0)
-  glVertex3f(-1.0, 1.0, 1.0)
-  glVertex3f(-1.0, 1.0, -1.0)
-
-  # Right face
-  glColor3f(1.0, 1.0, 0.0)  # Yellow
-  glVertex3f(1.0, -1.0, -1.0)
-  glVertex3f(1.0, 1.0, -1.0)
-  glVertex3f(1.0, 1.0, 1.0)
-  glVertex3f(1.0, -1.0, 1.0)
-
-  # Top face
-  glColor3f(1.0, 0.0, 1.0)  # Magenta
-  glVertex3f(-1.0, 1.0, -1.0)
-  glVertex3f(-1.0, 1.0, 1.0)
-  glVertex3f(1.0, 1.0, 1.0)
-  glVertex3f(1.0, 1.0, -1.0)
-
-  # Bottom face
-  glColor3f(0.0, 1.0, 1.0)  # Cyan
-  glVertex3f(-1.0, -1.0, -1.0)
-  glVertex3f(1.0, -1.0, -1.0)
-  glVertex3f(1.0, -1.0, 1.0)
-  glVertex3f(-1.0, -1.0, 1.0)
-
-  glEnd  # End drawing the cube
+  glEnd
 end
 
 def reshape(width, height)
-  glViewport(0, 0, width, height)  # Set the viewport
+  glViewport(0, 0, width, height)
   glMatrixMode(GL_PROJECTION)
   glLoadIdentity
 
-  # Set up perspective projection
+  # Use glFrustum to create a perspective projection
   aspect_ratio = width.to_f / height.to_f
   glFrustum(-aspect_ratio, aspect_ratio, -1.0, 1.0, 1.0, 100.0)
 
@@ -88,22 +55,30 @@ end
 
 def keyboard(key, x, y)
   case key
+  when 'w'
+    $y_move += 0.1
+  when 's'
+    $y_move -= 0.1
+  when 'a'
+    $x_move -= 0.1
+  when 'd'
+    $x_move += 0.1
   when 27  # ESC key
-    exit(0)  # Exit the program
+    exit 0
   end
 end
 
 def update(value)
-  $angle += 0.1  # Increment the rotation angle
+  $angle += 1.0
   glutPostRedisplay  # Redraw the scene
-  glutTimerFunc(16, :update, 0)  # Call update every 16 ms (~60 FPS)
+  glutTimerFunc(16, :update, 0)  # Call update every ~16ms (60 FPS)
 end
 
 # Initialize GLUT and OpenGL
 glutInit
 glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH)
 glutInitWindowSize(500, 500)
-glutCreateWindow("3D Moving Cube")
+glutCreateWindow("3D Moving Shapes")
 
 # Register callback functions
 init
@@ -111,7 +86,7 @@ glutDisplayFunc(:display)
 glutReshapeFunc(:reshape)
 glutKeyboardFunc(:keyboard)
 
-# Start the update loop
+# Use `glutTimerFunc` instead of `glutIdleFunc`
 glutTimerFunc(16, :update, 0)
 
 # Start the main loop
